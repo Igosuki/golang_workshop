f3a5f2459be4905a22720877ab5402305c7ef343
Guillaume Balaine <igosuki@gmail.com>
2015-01-30 10:31:17 +0100

    exo1 solution

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"net/http"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Hello World !")
+  var chttp = http.NewServeMux()
+
+  chttp.Handle("/", http.FileServer(http.Dir("./")))
+
+  http.HandleFunc("/", HomeHandler) // homepage
+  http.ListenAndServe(":8080", nil)
+
+  func HomeHandler(w http.ResponseWriter, r *http.Request) {
+    if (strings.Contains(r.URL.Path, ".")) {
+        chttp.ServeHTTP(w, r)
+    } else {
+        http.Error(w, "Not found", 404)
+    }
+  }
 }
+
